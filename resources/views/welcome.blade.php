a121fa5ab1b38b930568b8dbd1fa4506b3bf29f9
edmilsonmedeiros <edmilson.mprojetos@gmail.com>
2025-06-14 02:36:37 -0300

    update: home kanban screenshot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,21 @@
             <div class="col-lg-8 text-center">
                 <h1 class="logo mb-3"><i class="bi bi-kanban"></i> TaskFlow</h1>
                 <p class="subtitle mb-4">Gestor de Tarefas Kanban</p>
-                <p class="description mb-5">
+                <p class="description mb-5" style="font-size: 14px;">
                     Organize suas tarefas de forma visual e eficiente. Gerencie projetos, 
                     acompanhe o progresso e colabore com sua equipe usando o método Kanban.
                 </p>
                 
-                <div class="d-flex justify-content-center gap-3">
-                    <a href="/register" class="text-white mt-3 fw-bold fs-5">Cadastrar</a>
-                    <a href="/login" class="btn btn-light login-btn-custom text-white">Fazer Login</a>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex justify-content-center mt-1 mb-1">
+                        <img src="{{ asset('images/kanban.png') }}" alt="TaskFlow" class="img-fluid rounded-1" style="width: 75%;">
+                    </div>
+                    <div class="d-flex flex-column gap-3">
+                        <a href="/register" class="text-white mt-3 fw-bold fs-5">Cadastrar</a>
+                        <div>
+                            <a href="/login" class="btn btn-light login-btn-custom text-white">Login</a>
+                        </div>
+                    </div>
                 </div>
                 
                 <div class="row mt-5 mb-5 g-4">
