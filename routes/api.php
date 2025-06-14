fdb019cef5fd1e62bd97aa3da315fa1ae6833a6e
edmilsonmedeiros <edmilson.mprojetos@gmail.com>
2025-06-14 14:22:36 -0300

    update: improvments

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,3 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BoardController;
-
-Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::get('/boards', [BoardController::class, 'index']);
-    Route::post('/boards', [BoardController::class, 'store']);
-    Route::put('/boards/{id}', [BoardController::class, 'update']);
-    Route::delete('/boards/{id}', [BoardController::class, 'destroy']);
-});
